04ffdbcce4866579aa30ccdd25757ce293841a3a
“joaopaulotap” <joao_paulotap@hotmail.com>
2023-10-09 16:18:28 -0300

    advancing the project

--- src/componets/Sidebar.jsx
+++ src/componets/Sidebar.jsx
@@ -1,18 +1,24 @@
+import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 import perfilBanner from '../assets/profile-perfil-banner.jpg'
+import perfilImg from '../assets/profile-perfil-img.jpg'
 
 export function Sidebar () {
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src={perfilBanner} />
             <div className={styles.profile}>
+            <img className={styles.imgProfile} src={perfilImg} />
                 <strong>João Paulo Santos</strong>
                 <span>Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                <PencilLine size={20} />
+                Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
